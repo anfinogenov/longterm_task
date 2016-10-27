7a956cf96b87df00fc861dc460bfe6b6edc628ac
Maxim Anfinogenov <m.anfinogenov0@gmail.com>
2016-10-27 15:16:16 +0300

    refactoring stage 1
    Signed-off-by: Maxim Anfinogenov <m.anfinogenov0@gmail.com>

--- main.cxx
+++ main.cxx
@@ -9,14 +9,15 @@
 #include <pthread.h>
 #include <unistd.h>
 
-const char playerChar	= 'A';
-const char obstacleChar	= '#';
-const char wallChar	= 'X';
-const int  wallsWidth	= 49; // only mod2 = 1
-const long startTime	= time(NULL);
-const int  minLines	= 35; // exit if less or equal
-const int  dif_modifier	= 20; // 1 obstacle in (dif_modifier/difficulty) lines
+const char playerChar   = 'A';
+const char obstacleChar = '#';
+const char wallChar     = 'X';
+const int  wallsWidth   = 49; // only mod2 = 1
+const long startTime    = time(NULL);
+const int  minLines     = 35; // exit if less or equal
+const int  dif_modifier = 20; // 1 obstacle in (dif_modifier/difficulty) lines
 const int  counterFirstLn  = 3;
+const int  playerLives  = 15;
 
 static int fps = 15;
 static std::ofstream fout;
@@ -31,15 +32,15 @@ static bool pauseFlag = false;
 static int leftWall;
 static int rightWall;
 
-void*	multithread_movement (void* arg); 
-int 	movePlayer (const int & key);
-void	checkPlayer (int & local_x, int & local_y);
-void	logMessage (const std::string & msg, char msgType);
-void	printWalls (void);
+void*   multithread_movement (void* arg);
+int     movePlayer (const int & key);
+void    checkPlayer (int & local_x, int & local_y);
+void    logMessage (const std::string & msg, char msgType);
+void    printWalls (void);
 void    logCounter (void);
-void	generateNewLine (void);
-int	isWall (const int &x, const int &y);
-void	checkScreen (void);
+void    generateNewLine (void);
+bool    isWall (const int &x, const int &y);
+void    checkScreen (void);
 void    insertCounter (void);
 int     scoreAndExit (void);
 void    checkPause (void);
@@ -64,251 +65,264 @@ namespace bonus {
 
 int main () {
 
-////// initialization routines	
-	srand(time(NULL));
-        char* logFileName = (char*)malloc(50);
-        sprintf(logFileName, "logs/log-%lu.txt", time(NULL));
-        fout.open(logFileName);
-	if(!fout.is_open()) {
-		std::cerr << "cannot open log file\n";
-		exit(1);
-	}
-	if(!initscr()) {
-                logMessage ("initscr() failed", 'e');
-		exit(2);
-	}
-        logMessage ("initscr executed normally", 'n');
-        noecho();
-        curs_set(0);
-        raw();
-	keypad(stdscr, TRUE);
-        checkScreen();
-        if(!has_colors()) {
-                endwin();
-                logMessage ("colors isn't allowed in this terminal", 'e');
-                exit(5);
-        }
-        start_color();
+////// initialization routines
+    srand(time(NULL));
+    char logFileName[50] = {0};
+    sprintf(logFileName, "logs/log-%lu.txt", time(NULL));
+    fout.open(logFileName);
+    if(!fout.is_open()) {
+        std::cerr << "cannot open log file\n"; exit(1);
+    }
+    if(!initscr()) {
+        logMessage ("initscr() failed", 'e'); exit(2);
+    }
+    logMessage ("initscr executed normally", 'n');
+    noecho();
+    curs_set(0);
+    raw();
+    keypad(stdscr, TRUE);
+    checkScreen();
+    if(!has_colors()) {
+        endwin();
+        logMessage ("colors isn't allowed in this terminal", 'e');
+        exit(5);
+    }
+    start_color();
 ////// end of routines
 
 ////// variable initialization
-        leftWall = (COLS/2) - wallsWidth/2 - 1;
-        rightWall = (COLS/2) + wallsWidth/2 + 1;
-        global_player_x = LINES - 16;
-	global_player_y = COLS/2;
-	int local_player_x = global_player_x;
-	int local_player_y = global_player_y;
-        init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(2, COLOR_RED, COLOR_BLACK);
-        init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
-        init_pair(4, COLOR_GREEN, COLOR_BLACK);
-////// end of vars
-	
-	pthread_t move_thread;
-	pthread_create(&move_thread, NULL, multithread_movement, NULL);
+    leftWall = (COLS/2) - wallsWidth/2 - 1;
+    rightWall = (COLS/2) + wallsWidth/2 + 1;
+    global_player_x = LINES - playerLives - 1; //player starts with playerLives lines above bottom
+    global_player_y = COLS/2;
+    int local_player_x = global_player_x;
+    int local_player_y = global_player_y;
+////// end of variable initialization
 
-        clear(); refresh();
+////// color pairs initialization
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(4, COLOR_GREEN, COLOR_BLACK);
+////// end of color pairs initialization
 
-        while (!exitFlag) {
-                checkPause();
-                if (!pauseFlag) {
-                        if (!(counter % 300)) { difficulty++; bonus::rate += bonus::rateModifier; }
-                        points += (float)(difficulty*difficulty)/dif_modifier;
-                        mvaddch(local_player_x, local_player_y, ' ');
-                        printWalls(); //adds walls to refresh buffer
-                        refresh(); //puts obstacles on screen
-                        checkPlayer(local_player_x, local_player_y);
-                        if (local_player_x != global_player_x || local_player_y != global_player_y) {
-                                local_player_x = global_player_x;
-                                local_player_y = global_player_y;
-                        }
-                        mvaddch(local_player_x, local_player_y, playerChar | A_BOLD);
-                }
-                refresh(); //put changes on screen
-		napms(1000/fps); //make moves fps times per second
-		flushinp(); //remove any unattended input
-	}
-	pthread_cancel(move_thread);
-        if(scoreAndExit()) {
-            endwin();
-            logCounter();
-            fout.close();
-            system("./dd3o.exe");
+    pthread_t move_thread;
+    pthread_create(&move_thread, NULL, multithread_movement, NULL); //starts movePlayer-func in other thread
+
+    clear();
+
+    while (!exitFlag) {
+        checkPause();
+        if (!pauseFlag) {
+            if (!(counter % 300)) { //if counter%300 == 0 increase difficulty
+                clear();
+                mvprintw(counterFirstLn, rightWall + 3, "Level %d!", ++difficulty);
+                bonus::rate += bonus::rateModifier;
+            }
+            points += (float)(difficulty*difficulty)/dif_modifier;
+            //every tick increases points, depending on current difficulty
+            mvaddch(local_player_x, local_player_y, ' '); //erases old player
+            printWalls(); //adds walls to refresh buffer
+            refresh(); //puts buffer on screen
+            checkPlayer(local_player_x, local_player_y); //checks if there are any collisions with player
+            if (local_player_x != global_player_x || local_player_y != global_player_y) {
+                local_player_x = global_player_x;
+                local_player_y = global_player_y; //changes player coords if not equal
+            }
+            mvaddch(local_player_x, local_player_y, playerChar | A_BOLD); //prints new player
         }
-	endwin(); //closes curses screen
+        refresh(); //put changes on screen
+        napms(1000/fps); //make moves fps times per second
+        flushinp(); //remove any unattended input
+    }
+    pthread_cancel(move_thread);
+    if(scoreAndExit()) {
+        endwin();
         logCounter();
-        logMessage ("program exited normally", 'n');
-	fout.close();
-        printf("\x1b[0m");
+        fout.close();
+        system("./dd3o.exe");
+    }
+    endwin(); //closes curses screen
+    logCounter();
+    logMessage ("program exited normally", 'n');
+    fout.close();
+    printf("\x1b[0m"); //disables any color modificators
 
-	return 0;
+    return 0;
 }
 
 void* multithread_movement (void* arg) {
-	int key;
-        while (key = getch()) {
-		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-                if (!pauseFlag) movePlayer (key);
-		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    int key;
+    while (key = getch()) {
+        pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+        if (!pauseFlag) movePlayer (key); //disable moving if paused
+        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 
-		if (tolower(key) =='q') exitFlag = true;
-                if (tolower(key) == ' ') pauseFlag = !pauseFlag;
-		flushinp(); // removes any unattended input
-		napms(1000/fps);
-	}
-	return NULL;
+        if (tolower(key) =='q') exitFlag = true;
+        if (tolower(key) == ' ') pauseFlag = !pauseFlag;
+        flushinp(); // removes any unattended input
+        napms(1000/fps);
+    }
+    return NULL;
 }
 
 void checkPlayer (int & local_x, int & local_y) {
-        move (local_x, local_y);
-        char checkedPoint = inch();
-        if (checkedPoint != ' ')
-                switch (checkedPoint) {
-                case bonus::hpUpChar:
-                        addch(' ');
-                        bonus::hpUp();
-                        break;
-                case bonus::hpDownChar:
-                        addch(' ');
-                        bonus::hpDown();
-                        break;
-                case bonus::shootChar:
-                        addch(' ');
-                        bonus::shoot();
-                        break;
-                case obstacleChar:
-                        global_player_x++;
-                        break;
-                default:
-                        break;
-                }
+    move (local_x, local_y);
+    char checkedPoint = inch();
+    if (checkedPoint != ' ')
+        switch (checkedPoint) {
+            case bonus::hpUpChar:
+                addch(' ');
+                bonus::hpUp();
+                break;
+            case bonus::hpDownChar:
+                addch(' ');
+                bonus::hpDown();
+                break;
+            case bonus::shootChar:
+                addch(' ');
+                bonus::shoot();
+                break;
+            case obstacleChar:
+                global_player_x++;
+                break;
+            default:
+                break;
+        }
 
-        if (global_player_x >= LINES - 1) { //global
-                logMessage ("player fell out", 'n');
-		exitFlag = true;
-	}
+    if (global_player_x >= LINES - 1) { //global
+        logMessage ("player fell out", 'n');
+        exitFlag = true;
+    }
 }
 
 int movePlayer (const int & key) {
-	int new_coord; //temp variable to store coordinate where player wants to move in
-        switch (key) {
-		case KEY_RIGHT:
-			new_coord = global_player_y + 1;
-			if (!isWall(global_player_x, new_coord)) global_player_y = new_coord;
-			break;
-		case KEY_LEFT:
-			new_coord = global_player_y - 1;
-			if (!isWall(global_player_x, new_coord)) global_player_y = new_coord;
-			break;
-		default: break;
-	}
+    int new_coord; //temp variable to store coordinate where player wants to move in
+    switch (key) {
+        case KEY_RIGHT:
+            new_coord = global_player_y + 1;
+            if (!isWall(global_player_x, new_coord))
+                global_player_y = new_coord;
+            break;
+        case KEY_LEFT:
+            new_coord = global_player_y - 1;
+            if (!isWall(global_player_x, new_coord))
+                global_player_y = new_coord;
+            break;
+        default: break;
+    }
 }
 
 void printWalls () {
-	generateNewLine();
-	for (int i = 0; i < LINES; i++) {
-                mvaddch(i, leftWall, wallChar | COLOR_PAIR(1));
-                mvaddch(i, rightWall, wallChar | COLOR_PAIR(1));
-	}
-        for (int i = 1; i <= wallsWidth; i++)
-                mvaddch(LINES-1,i+leftWall, wallChar | COLOR_PAIR(2) | A_DIM);
+    generateNewLine();
+    for (int i = 0; i < LINES; i++) {
+        mvaddch(i, leftWall, wallChar | COLOR_PAIR(1));
+        mvaddch(i, rightWall, wallChar | COLOR_PAIR(1));
+    }
+    for (int i = 1; i <= wallsWidth; i++)
+        mvaddch(LINES-1,i+leftWall, wallChar | COLOR_PAIR(2) | A_DIM);
 }
 
 void generateNewLine () {
-        move(0, 0); insertln();
-        insertCounter();
-        if (!(counter++ % (dif_modifier/difficulty))) {
-                int len = rand()%wallsWidth/4;
-                int start = rand()%wallsWidth + leftWall;
-                for (int i = 0; i < len; i++) {
-                        if (start + i < rightWall) mvaddch(0, start + i, obstacleChar);
-                        if (start - i > leftWall) mvaddch(0, start - i, obstacleChar);
-                }
+    move(0, 0); insertln();
+    insertCounter();
+    if (!(counter++ % (dif_modifier/difficulty))) {
+        int len = rand()%wallsWidth/4;
+        int start = rand()%wallsWidth + leftWall;
+        for (int i = 0; i < len; i++) {
+            if (start + i < rightWall) mvaddch(0, start + i, obstacleChar);
+            if (start - i > leftWall) mvaddch(0, start - i, obstacleChar);
         }
-        else if (!(counter % bonus::rate)) bonus::generateBonus();
+    }
+    else if (!(counter % bonus::rate)) bonus::generateBonus();
 }
 
 void insertCounter () {
-        int secondLn = counterFirstLn+1;
-        for (int i = 0; i < leftWall; i++) {
-            mvaddch(secondLn, i, ' ');
-            mvaddch(secondLn+1, i, ' ');
-            mvaddch(secondLn+3, i, ' ');
-            mvaddch(secondLn+4, i, ' ');
-            mvaddch(secondLn+6, i, ' ');
-            mvaddch(secondLn+7, i, ' ');
-            mvaddch(secondLn+9, i, ' ');
-            mvaddch(secondLn+10, i, ' ');
-        }
-        mvprintw(counterFirstLn, leftWall - 13, "Points:");
-        mvprintw(counterFirstLn+1, leftWall - 13, "%d", (int)points);
-        mvprintw(counterFirstLn+3, leftWall - 13, "Time:");
-        mvprintw(counterFirstLn+4, leftWall - 13, "%d", time(NULL) - startTime);
-        mvprintw(counterFirstLn+6, leftWall - 13, "Difficulty:");
-        mvprintw(counterFirstLn+7, leftWall - 13, "%d", difficulty);
-        mvprintw(counterFirstLn+9, leftWall - 13, "Lives:");
-        mvprintw(counterFirstLn+10, leftWall - 13, "%d", LINES - global_player_x - 1);
+    int secondLn = counterFirstLn+1;
+    for (int i = 0; i < leftWall; i++) {
+        mvaddch(secondLn, i, ' ');
+        mvaddch(secondLn+1, i, ' ');
+        mvaddch(secondLn+3, i, ' ');
+        mvaddch(secondLn+4, i, ' ');
+        mvaddch(secondLn+6, i, ' ');
+        mvaddch(secondLn+7, i, ' ');
+        mvaddch(secondLn+9, i, ' ');
+        mvaddch(secondLn+10, i, ' ');
+    }
+    mvprintw(counterFirstLn,    leftWall - 13, "Points:");
+    mvprintw(counterFirstLn+1,  leftWall - 13, "%d", (int)points);
+    mvprintw(counterFirstLn+3,  leftWall - 13, "Time:");
+    mvprintw(counterFirstLn+4,  leftWall - 13, "%d", time(NULL) - startTime);
+    mvprintw(counterFirstLn+6,  leftWall - 13, "Difficulty:");
+    mvprintw(counterFirstLn+7,  leftWall - 13, "%d", difficulty);
+    mvprintw(counterFirstLn+9,  leftWall - 13, "Lives:");
+    mvprintw(counterFirstLn+10, leftWall - 13, "%d", LINES - global_player_x - 1);
 
-        return;
+    return;
 }
 
-int isWall (const int &x, const int &y) {
-        move(x, y);
-        char checkedPoint = inch();
-        if(checkedPoint == wallChar || checkedPoint == obstacleChar) return 1;
-        return 0;
+bool isWall(const int &x, const int &y) {
+    move(x, y);
+    char checkedPoint = inch();
+    if(checkedPoint == wallChar || checkedPoint == obstacleChar)
+        return true;
+    return false;
 }
 
 void logMessage (const std::string & msg, char msgType) {
-	std::string type;
-	switch (msgType) { //converting char msgType to string
-		case 'n': type = "okay"; break;
-		case 'w': type = "warn"; break;
-		case 'e': type = "err "; break;
-		default : type = "unkn"; break;
-	}
-	fout << type + " [" << time(NULL) - startTime << "]: " << msg << std::endl;
-	//out message with msgType and time
+    std::string msg_type;
+    switch (msgType) { //converting char msgType to string
+        case 'n':
+            msg_type = "okay";
+            break;
+        case 'w':
+            msg_type = "warn";
+            break;
+        case 'e':
+            msg_type = "err ";
+            break;
+        default :
+            msg_type = "unkn";
+            break;
+    }
+    fout << msg_type + " [" << time(NULL) - startTime << "]: " << msg << std::endl;
+    //out message with msgType and time from start
 }
 
 void checkScreen () {
-        if (COLS < (wallsWidth+30) || LINES < minLines) {
-                logMessage ("incorrect screen size", 'e');
-		endwin();
-		exit (4);
-	}
-        logMessage ("screen size is correct", 'n');
+    if (COLS < (wallsWidth+30) || LINES < minLines) {
+        logMessage ("incorrect screen size", 'e');
+        endwin();
+        exit (4);
+    }
+    logMessage ("screen size is correct", 'n');
 }
 
 void logCounter () {
-        char temp[25] = {0};
-        sprintf(temp, "counter is %d", counter);
-        logMessage (temp, 'n');
-        sprintf(temp, "points is %d", (int)points);
-        logMessage (temp, 'n');
+    char temp[25] = {0};
+    sprintf(temp, "counter is %d", counter);
+    logMessage (temp, 'n');
+    sprintf(temp, "points is %d", (int)points);
+    logMessage (temp, 'n');
 
 }
 
 int scoreAndExit() {
-        char exitkey;
-        mvprintw (LINES/2 - 1, COLS/2 - 17, "   Game over: Your score is %d   ", (int)points);
-        mvprintw (LINES/2    , COLS/2 - 11, "   Press 'q' to exit   ");
-        mvprintw (LINES/2 + 1, COLS/2 - 15, "   or press 'r' to restart   ");
-        while (tolower(exitkey = getch()) != 'q') if (exitkey == 'r') return 1; //awaits for 'q' to  exit
-        return 0;
+    char exitkey;
+    mvprintw (LINES/2 - 1, COLS/2 - 17, "   Game over: Your score is %d   ", (int)points);
+    mvprintw (LINES/2    , COLS/2 - 11, "   Press 'q' to exit   ");
+    mvprintw (LINES/2 + 1, COLS/2 - 15, "   or press 'r' to restart   ");
+    while (tolower(exitkey = getch()) != 'q') if (exitkey == 'r') return 1; //awaits for 'q' to  exit
+    return 0;
 }
 
 void bonus::shoot () {
     for (int i = global_player_x-1; i > 0; i--) {
-        move (i, global_player_y);
-        if(inch() == obstacleChar)
-            addch (' ');
-        move (i, global_player_y-1);
-        if(inch() == obstacleChar)
-            addch (' ');
-        move (i, global_player_y+1);
-        if(inch() == obstacleChar)
-            addch (' ');
+        for (int y = global_player_y-1; y <= global_player_y+1; y++) {
+            move (i, y);
+            if(inch() == obstacleChar)
+                addch (' ');
+        }
     }
 }
 
@@ -323,17 +337,17 @@ void bonus::hpDown () {
 void bonus::generateBonus () {
     int bonus_x = leftWall + rand()%wallsWidth;
     switch (rand()%3) {
-    case 0:
-        mvaddch(0, bonus_x, bonus::shootChar | COLOR_PAIR(3));
-        break;
-    case 1:
-        mvaddch(0, bonus_x, bonus::hpUpChar | COLOR_PAIR(4));
-        break;
-    case 2:
-        mvaddch(0, bonus_x, bonus::hpDownChar | COLOR_PAIR(2));
-        break;
-    default:
-        break;
+        case 0:
+            mvaddch(0, bonus_x, bonus::shootChar | COLOR_PAIR(3));
+            break;
+        case 1:
+            mvaddch(0, bonus_x, bonus::hpUpChar | COLOR_PAIR(4));
+            break;
+        case 2:
+            mvaddch(0, bonus_x, bonus::hpDownChar | COLOR_PAIR(2));
+            break;
+        default:
+            break;
     }
 }
 
